1880a56011b637e5fe76c3d088c0edea7b8679d3
Kartikesh Nadar <KartikeshN@microland.com>
2020-03-25 15:15:18 +0530

    Exercise 12 Completed

---
+++ lpthw/ex12.py
@@ -0,0 +1,9 @@
+# Prompting People
+
+age = input("How old are you (yrs)? ")
+height = input("How tall are you (cms)? ")
+weight = input("What is your weight (kgs)? ")
+
+print(f"So you are {age} yrs old, {height} cms tall and {weight} kgs heavy.")
+
+print("My name is", input("What is your nickname? "))
