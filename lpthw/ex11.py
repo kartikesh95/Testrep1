482993bedb4b4c3462e412ba5a3544b8f1c446a8
Kartikesh Nadar <KartikeshN@microland.com>
2020-03-25 13:13:43 +0530

    Exercise 11 Completed

---
+++ lpthw/ex11.py
@@ -0,0 +1,10 @@
+# Asking Questions
+
+print('How old are you?', end='')
+age = input()
+print('How tall are you (in inches)?', end=' ')
+height = input()
+print("How much do you weigh (in kgs)?", end=' ')
+weight = input()
+
+print(f"So you are {age} old, {height} tall and {weight} heavy.")
